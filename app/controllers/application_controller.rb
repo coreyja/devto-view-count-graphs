a9673efee3eb9f00b0ff862cab2dcf04ca66606f
Corey Alexander <coreyja@gmail.com>
2020-12-15 18:42:16 -0500

    Use basic auth for all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with(
+    name: ENV.fetch('BASIC_AUTH_USERNAME'),
+    password: ENV.fetch('BASIC_AUTH_PASSWORD'),
+  )
 end
