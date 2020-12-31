d6a50c0454280e27eaa6f9a7784d597bf75d99b0
Corey Alexander <coreyja@gmail.com>
2020-12-30 21:43:49 -0500

    Make the expandable list a component and make it more button looking

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,9 @@ class ArticlesController < ApplicationController
     @most_recent_articles = Article.all.order(published_at: :desc).limit(3)
     now = Time.zone.now
     @articles_this_month = Article.where(published_at: (now.beginning_of_month..now.end_of_month)).order(published_at: :desc)
-    # previous_start = now.beginning_of_month
-    # @articles_in_preview_3_months = Article.where(published_at: (Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)).order(published_at: :desc)
+    previous_start = now.beginning_of_month - 4.months
+    @articles_in_preview_3_months = Article.where(published_at: (previous_start...now.beginning_of_month)).order(published_at: :desc)
+    @older_articles = Article.where('published_at < ?', previous_start).order(published_at: :desc)
   end
 
   def show
--- app/javascript/controllers/expandable_list_controller.ts
+++ app/javascript/controllers/expandable_list_controller.ts
@@ -1,11 +1,24 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "list" ]
+  static targets = [ "list", "icon" ]
+  static values = { expanded: Boolean }
 
   declare listTarget: HTMLUListElement
+  declare iconTarget: HTMLElement
+  declare expandedValue: Boolean
 
   toggle() {
-    this.listTarget.classList.toggle("hidden");
+    this.expandedValue = !this.expandedValue
+  }
+
+  expandedValueChanged() {
+    if (this.expandedValue) {
+      this.listTarget.classList.remove("hidden")
+      this.iconTarget.classList.add("rotate-90")
+    } else {
+      this.listTarget.classList.add("hidden")
+      this.iconTarget.classList.remove("rotate-90")
+    }
   }
 }
