6d462427fd59c482f32a39bfb18174d587eb8861
Corey Alexander <coreyja@gmail.com>
2020-12-31 15:41:24 -0500

    Route to new pages and delete old show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'articles#index'
-  resources :articles, only: %w[index show] do
+  resources :articles, only: %w[index] do
     resource :page_views, only: %w[show]
     resource :likes, only: %w[show]
     resource :comments, only: %w[show]
