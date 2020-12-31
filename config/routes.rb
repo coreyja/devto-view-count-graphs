95d9d02916eb761b27d4bfbe5ee8dca709844ba2
Corey Alexander <coreyja@gmail.com>
2020-12-31 15:35:14 -0500

    Create a View Graph page thats a bit cleaner

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'articles#index'
-  resources :articles
+  resources :articles, only: %w[index show] do
+    resource :page_views, only: %w[show]
+  end
 end
