93c1fbfd9db04d9eb20c0c83e2017970308dca4c
Corey Alexander <coreyja@gmail.com>
2020-12-31 21:29:21 -0500

    Add little profile icon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
   http_basic_authenticate_with(
     name: ENV.fetch('BASIC_AUTH_USERNAME'),
     password: ENV.fetch('BASIC_AUTH_PASSWORD')
