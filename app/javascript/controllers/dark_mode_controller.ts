6190fe57148493ca2efe0d75319f2a90ec2655f3
Corey Alexander <coreyja@gmail.com>
2021-01-01 15:55:29 -0500

    Add Dark Mode Toggle

--- app/javascript/controllers/dark_mode_controller.ts
+++ app/javascript/controllers/dark_mode_controller.ts
@@ -1,11 +1,14 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "html" ]
+  static targets = [ "html", "toggle", "sunIcon", "moonIcon" ]
   static values = { darkEnabled: Boolean }
 
   declare darkEnabledValue: Boolean
   declare htmlTarget: HTMLElement
+  declare toggleTarget: HTMLElement
+  declare sunIconTarget: HTMLElement
+  declare moonIconTarget: HTMLElement
 
   toggle() {
     this.darkEnabledValue = !this.darkEnabledValue
@@ -14,8 +17,14 @@ export default class extends Controller {
   darkEnabledValueChanged() {
     if (this.darkEnabledValue) {
       this.htmlTarget.classList.add('dark')
+      this.toggleTarget.classList.add('left-1/2')
+      this.sunIconTarget.classList.add('opacity-0')
+      this.moonIconTarget.classList.remove('opacity-0')
     } else {
       this.htmlTarget.classList.remove('dark')
+      this.toggleTarget.classList.remove('left-1/2')
+      this.sunIconTarget.classList.remove('opacity-0')
+      this.moonIconTarget.classList.add('opacity-0')
     }
   }
 }
