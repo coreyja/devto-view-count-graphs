6364326c02e0fd27fbb8126cded6e42d6e877a9d
Corey Alexander <coreyja@gmail.com>
2020-12-31 19:22:12 -0500

    Get a header created and some colors and styling happening

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -10,6 +10,22 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      blurple: {
+        lighter: '#8990f9',
+        light: '#5b64ef',
+        DEFAULT: '#3E49D7',
+      },
+      yellow: {
+        DEFAULT: '#F7CB45'
+      },
+      pink: {
+        DEFAULT: '#F3CBFC'
+      },
+      white: {
+        DEFAULT: '#FFF',
+      }
+    }
   },
   variants: {
     extend: {},
