ad68721a250a47903cc3081137bc88e339fcc559
Corey Alexander <coreyja@gmail.com>
2021-01-10 21:39:48 -0500

    Get footer to keep the date range

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,45 +1,8 @@
 # frozen_string_literal: true
 
 class LikesController < ApplicationController
-  class Presenter
-    include ActiveModel::Model
-
-    attr_accessor :article
-
-    def start_at
-      @start_at || min_start_at
-    end
-
-    def end_at
-      @end_at || max_end_at
-    end
-
-    def start_at=(val)
-      val = Time.zone.parse(val) if val.is_a?(String)
-      @start_at = val
-    end
-
-    def end_at=(val)
-      val = Time.zone.parse(val) if val.is_a?(String)
-      @end_at = val
-    end
-
-    def article_stats
-      article.article_stats.where(recorded_at: start_at.beginning_of_day..end_at.end_of_day)
-    end
-
-    def min_start_at
-      article.article_stats.order(recorded_at: :asc).limit(1).pluck(:recorded_at).first.to_date
-    end
-
-    def max_end_at
-      Time.zone.today
-    end
-  end
-
   def show
-    @article = Article.find(params[:article_id])
-    @article_stats = @article.article_stats
-    @presenter = Presenter.new(params.permit(:start_at, :end_at).merge(article: @article))
+    article = Article.find(params[:article_id])
+    @presenter = GraphPresenter.new(params.permit(:start_at, :end_at).merge(article: article))
   end
 end
---
+++ app/models/graph_presenter.rb
@@ -0,0 +1,40 @@
+class GraphPresenter
+  include ActiveModel::Model
+  include ActionView::Helpers::DateHelper
+
+  attr_accessor :article
+
+  def start_at
+    @start_at || min_start_at
+  end
+
+  def end_at
+    @end_at || max_end_at
+  end
+
+  def start_at=(val)
+    val = Time.zone.parse(val) if val.is_a?(String)
+    @start_at = val
+  end
+
+  def end_at=(val)
+    val = Time.zone.parse(val) if val.is_a?(String)
+    @end_at = val
+  end
+
+  def article_stats
+    article.article_stats.where(recorded_at: start_at.beginning_of_day..end_at.end_of_day)
+  end
+
+  def min_start_at
+    article.article_stats.order(recorded_at: :asc).limit(1).pluck(:recorded_at).first.to_date
+  end
+
+  def max_end_at
+    Time.zone.today
+  end
+
+  def timespan_in_words
+    distance_of_time_in_words(start_at.beginning_of_day, end_at.end_of_day)
+  end
+end
