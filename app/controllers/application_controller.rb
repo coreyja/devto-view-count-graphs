3502a051e67f4f4d17ce159ce3265e4fc83d70e4
Corey Alexander <coreyja@gmail.com>
2021-01-11 00:20:15 -0500

    Only do basic auth if the env vars are there

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,12 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
 
-  http_basic_authenticate_with(
-    name: ENV.fetch('BASIC_AUTH_USERNAME'),
-    password: ENV.fetch('BASIC_AUTH_PASSWORD')
-  )
+  if ENV['BASIC_AUTH_USERNAME'].present? && ENV['BASIC_AUTH_PASSWORD'].present?
+    http_basic_authenticate_with(
+      name: ENV.fetch('BASIC_AUTH_USERNAME'),
+      password: ENV.fetch('BASIC_AUTH_PASSWORD')
+    )
+  end
 
   private
 
