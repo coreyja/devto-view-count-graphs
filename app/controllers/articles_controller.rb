e9373815e3f40036e5bf557108169eff5fc9cca5
Corey Alexander <coreyja@gmail.com>
2021-01-11 00:05:31 -0500

    Fix and ignore the last rubo violations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
-  def index
+  def index # rubocop:disable Metrics/AbcSize
     @most_recent_articles = Article.all.order(published_at: :desc).limit(3)
     now = Time.zone.now
-    @articles_this_month = Article.where(published_at: (now.beginning_of_month..now.end_of_month)).order(published_at: :desc)
+    @articles_this_month = Article
+                           .where(published_at: (now.beginning_of_month..now.end_of_month))
+                           .order(published_at: :desc)
     previous_start = now.beginning_of_month - 4.months
-    @articles_in_preview_3_months = Article.where(published_at: (previous_start...now.beginning_of_month)).order(published_at: :desc)
+    @articles_in_preview_3_months = Article
+                                    .where(published_at: (previous_start...now.beginning_of_month))
+                                    .order(published_at: :desc)
     @older_articles = Article.where('published_at < ?', previous_start).order(published_at: :desc)
   end
 
